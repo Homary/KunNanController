ec3e94faaf428e9a4648a404055d86e6858992cf
Homary <935124541@qq.com>
2019-07-26 16:52:27 +0800

    fixed: 修复密码框密码可复制问题

--- web/src/views/login/login.vue
+++ web/src/views/login/login.vue
@@ -1,20 +1,37 @@
 <template>
-<div class="login-main">
-	<section class="login-main-wrapper">
-		<h3>{{login_title}}</h3>
-		<div class="form">
-			<label for="user" class="input-wrap"><i class="icon-user"></i><input type="text" id="user" readonly="true" unselectable="on" onfocus="this.blur()" v-model="admin"/></label>
-			<label for="password" class="input-wrap"><i class="icon-pwd"></i><input type="password" id="password" v-model="pwd" required="required" @keyup.enter="setForm"/></label>
-			<button class="btn" @click="setForm" >登陆</button>
-		</div>
-	</section>
-</div>
+	<div class="login-main">
+		<section class="login-main-wrapper">
+			<h3>{{ login_title }}</h3>
+			<div class="form">
+				<label for="user" class="input-wrap"
+					><i class="icon-user"></i
+					><input
+						type="text"
+						id="user"
+						readonly="true"
+						unselectable="on"
+						onfocus="this.blur()"
+						v-model="admin" 
+                        oncopy="return false;"
+				/></label>
+				<label for="password" class="input-wrap"
+					><i class="icon-pwd"></i
+					><input
+						type="password"
+						id="password"
+						v-model="pwd"
+						required="required"
+						@keyup.enter="setForm" 
+                        oncopy="return false;"
+				/></label>
+				<button class="btn" @click="setForm">登陆</button>
+			</div>
+		</section>
+	</div>
 </template> 
 
 <script src="./login.ts">
-
 </script>
 
 <style src="./login.less" lang="less">
-
 </style>
